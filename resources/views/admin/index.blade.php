1d4c8f12cd91f7df2057936beca6fb1b1fb26f8e
AngelAld <aaaldana50@gmail.com>
2025-05-30 18:05:09 -0500

    Update routes and add admin view: change dashboard route to admin and create admin index view

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,12 @@
+{{-- resources/views/tu-vista.blade.php --}}
+@extends('adminlte::page')
+
+@section('title', 'Título de la Página')
+
+@section('content_header')
+    <h1>AdminLTE</h1>
+@stop
+
+@section('content')
+    <p>Proyecto de apoyo al recojo de residuos solidos</p>
+@stop
