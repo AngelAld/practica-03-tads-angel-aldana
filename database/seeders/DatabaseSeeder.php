79828654d6dd614aa30a8330f6014eb46468be4e
AngelAld <aaaldana50@gmail.com>
2025-05-30 17:14:14 -0500

    Fix casing of EmployeeTypeSeeder in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,7 +23,7 @@ class DatabaseSeeder extends Seeder
             DepartmentSeeder::class,
             ProvinceSeeder::class,
             DistrictSeeder::class,
-            EmployeeTypeSeeder::class,
+            EmployeetypeSeeder::class,
             VehicletypeSeeder::class,
             ContracttypeSeeder::class,
             ScheduleShiftSeeder::class,
